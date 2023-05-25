8849cd210f101df37325786a94fb58afa9002fa3
Beyang Liu <beyang@sourcegraph.com>
2023-05-25 16:59:35 -0700

    fast file finder

--- client/cody/src/chat/ChatViewProvider.ts
+++ client/cody/src/chat/ChatViewProvider.ts
@@ -31,6 +31,7 @@ import { LocalStorage } from '../services/LocalStorageProvider'
 import { CODY_ACCESS_TOKEN_SECRET, SecretStorage } from '../services/SecretStorageProvider'
 import { TestSupport } from '../test-support'
 
+import { fastFilesExist } from './fastFileFinder'
 import { ConfigurationSubsetForWebview, DOTCOM_URL, ExtensionMessage, WebviewMessage } from './protocol'
 
 export async function isValidLogin(
@@ -266,7 +267,14 @@ export class ChatViewProvider implements vscode.WebviewViewProvider, vscode.Disp
                 const lastInteraction = this.transcript.getLastInteraction()
                 if (lastInteraction) {
                     const displayText = reformatBotMessage(text, responsePrefix)
-                    const { text: highlightedDisplayText } = await highlightTokens(displayText || '', filesExist)
+                    const fileExistFunc = (filePaths: string[]): Promise<{ [filePath: string]: boolean }> => {
+                        const rootPath = this.editor.getWorkspaceRootPath()
+                        if (!rootPath) {
+                            return Promise.resolve({})
+                        }
+                        return fastFilesExist(this.rgPath, rootPath, filePaths)
+                    }
+                    const { text: highlightedDisplayText } = await highlightTokens(displayText || '', fileExistFunc)
                     this.transcript.addAssistantResponse(text || '', highlightedDisplayText)
                     this.editor.controller.reply(highlightedDisplayText)
                 }
@@ -715,40 +723,6 @@ export class ChatViewProvider implements vscode.WebviewViewProvider, vscode.Disp
         }
         this.disposables = []
     }
-}
-
-function filePathContains(container: string, contained: string): boolean {
-    let trimmedContained = contained
-    if (trimmedContained.endsWith(path.sep)) {
-        trimmedContained = trimmedContained.slice(0, -path.sep.length)
-    }
-    if (trimmedContained.startsWith(path.sep)) {
-        trimmedContained = trimmedContained.slice(path.sep.length)
-    }
-    if (trimmedContained.startsWith('.' + path.sep)) {
-        trimmedContained = trimmedContained.slice(1 + path.sep.length)
-    }
-    return (
-        container.includes(path.sep + trimmedContained + path.sep) || // mid-level directory
-        container.endsWith(path.sep + trimmedContained) // child
-    )
-}
-
-async function filesExist(filePaths: string[]): Promise<{ [filePath: string]: boolean }> {
-    const searchPath = `{${filePaths.join(',')}}`
-    const realFiles = await vscode.workspace.findFiles(searchPath, null, filePaths.length * 5)
-    const ret: { [filePath: string]: boolean } = {}
-    for (const filePath of filePaths) {
-        let pathExists = false
-        for (const realFile of realFiles) {
-            if (filePathContains(realFile.fsPath, filePath)) {
-                pathExists = true
-                break
-            }
-        }
-        ret[filePath] = pathExists
-    }
-    return ret
 }
 
 // Converts a git clone URL to the codebase name that includes the slash-separated code host, owner, and repository name
